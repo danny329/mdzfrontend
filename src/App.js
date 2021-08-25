4fd97b49cd61a1917abb4074585ab87812f474ad
danny329 <danielpaul329@outlook.com>
2021-08-25 23:25:18 +0530

    websocket send and receive completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+import {useState} from 'react';
 import './App.css';
-
+const SOCKET_URL = "http://127.0.0.1:8080/handler";
+var stompClient = null;
 function App() {
+  const [clientConnected, setclientConnected] = useState(false);
+  
+  var sock = new SockJS(SOCKET_URL);
+  stompClient = Stomp.over(sock);
+  stompClient.connect({}, function (frame) {
+      setclientConnected(true);
+      console.log('Connected: ' + frame);
+      stompClient.subscribe('/topic/persons', function (data) {
+          console.log(JSON.parse(data.body).content);
+      });
+  });
+  sock.onopen = function() {
+      console.log('open');
+      // sock.send('test');
+  };
+ 
+  sock.onmessage = function(e) {
+      console.log('message', e.data);
+      // sock.close();
+  };
+ 
+  sock.onclose = function() {
+      setclientConnected(false);
+      console.log('close');
+  };
+ 
+  const clickHandler = () =>{
+    try {
+      stompClient.send("/app/persons", {}, JSON.stringify({id:1,name:'daniel',dob:'27/03/1996',salary:87955.00,age:27}));
+      return true;
+    } catch(e) {
+      return false;
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <button onClick={clickHandler}>click</button>
     </div>
   );
 }
